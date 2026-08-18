d00b9254bc06951ef9ca16f64bdede494dd52e39
Sergey Petrov <spetrov@soft2run.com>
2026-08-18 09:31:29 +0300

    rev

--- emulators/dmg/apu_structs.hpp
+++ emulators/dmg/apu_structs.hpp
@@ -126,7 +126,11 @@ struct WaveChannel {
 
     uint8_t volumeCode = 0;
     uint8_t wavePosition = 0;
-    std::array<uint8_t, 16> waveRAM {};
+    // https://gbdev.gg8.se/wiki/articles/Gameboy_sound_hardware#Power_Control
+    std::array<uint8_t, 16> waveRAM {
+        0x84, 0x40, 0x43, 0xAA, 0x2D, 0x78, 0x92, 0x3C,
+        0x60, 0x59, 0x59, 0xB0, 0x34, 0xB8, 0x2E, 0xDA
+    };
     uint8_t currentSample = 0;
     uint8_t sampleBuffer = 0;
 
